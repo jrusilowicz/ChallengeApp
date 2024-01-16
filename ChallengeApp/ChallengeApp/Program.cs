3f8f57cd5f0e28160389a3f4cbfa5057429f75df
jrusilowicz <jrusilowicz@gmail.com>
2024-01-16 13:41:57 +0100

    Zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witam Wszystkich, życzę Wam i sobie powodzenia !!!");
+﻿string name = "Ewa";
+char sex = 'K';
+int age = 17;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex != 'K' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
